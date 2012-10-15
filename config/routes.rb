ef075de096593430e82027b511bc81a0a80f2872
miketan <tky_mike@hotmail.com>
2012-10-15 18:35:21 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 SampleApp::Application.routes.draw do
 #  get "users/new"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
 
 #  get "static_pages/home"
 #  get "static_pages/help"
