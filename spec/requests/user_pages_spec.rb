c9aea36ba1d34eba0acecc643649b2103347e2a1
miketan <tky_mike@hotmail.com>
2012-10-15 15:19:15 +0800

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,39 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
-  	let(:user) { FactoryGirl.create(:user) }
+  # describe "signup page" do
+  # 	#let(:user) { FactoryGirl.create(:user) }
+  # 	let(:user) {User.find(:user)}
+  #   before { visit signup_path }
+
+  #   it { should have_selector('h1',    text: user.name) }
+  #   it { should have_selector('title', text: user.name) }
+  # end
+
+  describe "signup" do
+
     before { visit signup_path }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
+
 end
