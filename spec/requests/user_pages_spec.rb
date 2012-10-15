a64cfbe5f322c325048dc580ca668d49ded45abc
miketan <tky_mike@hotmail.com>
2012-10-15 11:05:01 +0800

    Test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,10 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
+  	let(:user) { FactoryGirl.create(:user) }
     before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 end
